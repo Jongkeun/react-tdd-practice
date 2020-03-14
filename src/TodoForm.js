c551e58cc859a615d3006516d120c19239ef733f
Jongkeun <dldkfod@naver.com>
2020-03-14 23:16:47 +0900

    UI 구성하기

---
+++ src/TodoForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoForm = () => {
+  return (
+    <div>
+      <input placeholder={"할 일을 입력하세요"} />
+      <button>등록</button>
+    </div>
+  );
+};
+
+export default TodoForm;
